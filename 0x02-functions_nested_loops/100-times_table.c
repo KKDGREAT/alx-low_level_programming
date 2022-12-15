c574f5d3bea4ebce42f8557bb4462d03e3c20d58
KKDGREAT <bisoye.koya@gmail.com>
2022-12-14 17:20:34 -0800

    task 12 again

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,11 +25,14 @@ else if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar(' ');
 _putchar(k + '0');
 }
 else if (k >= 10 && k < 100)
 {
 _putchar(',');
+_putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
@@ -44,6 +47,7 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
+}
 }
 }
 }
